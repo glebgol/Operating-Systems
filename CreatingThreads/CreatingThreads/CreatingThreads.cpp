7fa2740240f315e101c59bbd61799b1c01ad5a3c
Gleb Golovnev <glebgolovnev200@gmail.com>
2022-10-07 21:29:35 +0300

    Created Main thread

--- CreatingThreads/CreatingThreads/CreatingThreads.cpp
+++ CreatingThreads/CreatingThreads/CreatingThreads.cpp
@@ -62,6 +62,61 @@ DWORD WINAPI Average(LPVOID lpParameters)
 	return 0;
 }
 
+DWORD WINAPI Main(LPVOID lpParameters)
+{
+	std::cout << "Main thread is started." << std::endl;
+	std::cout << "Enter the size of array: ";
+	int size = 0;
+	std::cin >> size;
+	Array* array = new Array(size);
+	std::cout << "Enter " << size << " elements: ";
+	for (int i = 0; i < size; i++) {
+		std::cin >> array->p[i];
+	}
+	HANDLE hThread1;
+	DWORD IDThread1;
+	hThread1 = CreateThread(NULL, 0, MinMax, array, 0, &IDThread1);
+	if (hThread1 == NULL) {
+		return GetLastError();
+	}
+
+	HANDLE hThread2;
+	DWORD IDThread2;
+	hThread2 = CreateThread(NULL, 0, Average, array, 0, &IDThread2);
+	if (hThread2 == NULL) {
+		return GetLastError();
+	}
+
+	WaitForSingleObject(hThread1, INFINITE);
+	WaitForSingleObject(hThread2, INFINITE);
+	CloseHandle(hThread1);
+	CloseHandle(hThread2);
+
+	int indexOfMin = array->indexOfMin;
+	int indexOfMax = array->indexOfMax;
+	int average = array->average;
+
+	array->p[indexOfMin] = average;
+	array->p[indexOfMax] = average;
+
+	for (int i = 0; i < size; i++) {
+		std::cout << array->p[i] << ' ';
+	}
+	return 0;
+}
+
 int main()
 {
+	HANDLE hThread;
+	DWORD IDThread;
+	hThread = CreateThread(NULL, 0, Main, NULL, 0, &IDThread);
+
+	if (hThread == NULL) {
+		return GetLastError();
+	}
+
+	WaitForSingleObject(hThread, INFINITE);
+	CloseHandle(hThread);
+
+	return 0;
 }

6213f25e18d0e65f180dc51ddef3f8feb091e81b
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-19 11:52:31 +0300

    Sender is working

--- ProcessSynchronization/sender/sender.cpp
+++ ProcessSynchronization/sender/sender.cpp
@@ -1,20 +1,36 @@
-// sender.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <string>
+#include <cstdlib>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+CRITICAL_SECTION criticalSection;
+
+int main(int args, char* argv[]) {
+    InitializeCriticalSection(&criticalSection);
+    if (args == 2) {
+        HANDLE Event = OpenEventA(EVENT_MODIFY_STATE, FALSE, argv[0]);
+        HANDLE startEvent = OpenEventA(SYNCHRONIZE, FALSE, "StartSenderEvent");
+        std::ofstream fout(argv[1], std::ios_base::binary | std::ios_base::out | std::ios_base::app);
+        std::string message;
+        SignalObjectAndWait(Event, startEvent, INFINITE, FALSE);
+        bool ind = false;
+        while (!ind) {
+            printf("Input message to send or ");
+            printf("0");
+            printf(" to stop");
+            std::cin >> message;
+            if (strcmp("0", message.c_str()) != 0) {
+                EnterCriticalSection(&criticalSection);
+                fout << message << std::endl;
+                SetEvent(Event);
+                LeaveCriticalSection(&criticalSection);
+            }
+            else ind = true;
+        }
+        CloseHandle(startEvent);
+        fout.close();
+        CloseHandle(Event);
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

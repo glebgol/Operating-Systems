59cf7d73cecb5cad77bb9197fb9511cebb702e8d
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-19 11:53:41 +0300

    Add constants

--- ProcessSynchronization/sender/sender.cpp
+++ ProcessSynchronization/sender/sender.cpp
@@ -3,14 +3,16 @@
 #include <Windows.h>
 #include <string>
 #include <cstdlib>
-
+const char* EVENT_NAME = "StartSenderEvent";
+const int NUM_OF_ARGS = 2;
+const char* STOP_MESSAGE = "0";
 CRITICAL_SECTION criticalSection;
 
 int main(int args, char* argv[]) {
     InitializeCriticalSection(&criticalSection);
-    if (args == 2) {
+    if (args == NUM_OF_ARGS) {
         HANDLE Event = OpenEventA(EVENT_MODIFY_STATE, FALSE, argv[0]);
-        HANDLE startEvent = OpenEventA(SYNCHRONIZE, FALSE, "StartSenderEvent");
+        HANDLE startEvent = OpenEventA(SYNCHRONIZE, FALSE, EVENT_NAME);
         std::ofstream fout(argv[1], std::ios_base::binary | std::ios_base::out | std::ios_base::app);
         std::string message;
         SignalObjectAndWait(Event, startEvent, INFINITE, FALSE);
@@ -20,7 +22,7 @@ int main(int args, char* argv[]) {
             printf("0");
             printf(" to stop");
             std::cin >> message;
-            if (strcmp("0", message.c_str()) != 0) {
+            if (strcmp(STOP_MESSAGE, message.c_str()) != 0) {
                 EnterCriticalSection(&criticalSection);
                 fout << message << std::endl;
                 SetEvent(Event);
